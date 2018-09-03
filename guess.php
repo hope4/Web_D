e2f784d77b784aad42b3d882d0524df10d5921a4
Naveen <201451074@iiitvadodara.ac.in>
2018-09-03 13:07:09 +0530

    Week 6 assign.@coursera with ngork firewall tunnel

---
+++ guess.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>K Naveen Kumar 61a10e6a</title>
+</head>
+<body>
+<h1>Welcome to my guessing game</h1>
+<p>
+<?php
+  if ( ! isset($_GET['guess']) ) { 
+    echo("Missing guess parameter");
+  } else if ( strlen($_GET['guess']) < 1 ) {
+    echo("Your guess is too short");
+  } else if ( ! is_numeric($_GET['guess']) ) {
+    echo("Your guess is not a number");
+  } else if ( $_GET['guess'] < 55 ) {
+    echo("Your guess is too low");
+  } else if ( $_GET['guess'] > 55 ) {
+    echo("Your guess is too high");
+  } else {
+    echo("Congratulations - You are right");
+  }
+?>
+</p>
+</body>
+</html>
+  
+
+
